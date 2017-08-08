b71621ed37565d1e1f93a60fcc48e8f59993c918
Menq <sahak@ukdevplus.co.uk>
2017-08-09 00:47:05 +0400

    update

--- src/Resources/Views/settings.blade.php
+++ src/Resources/Views/settings.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.mTabs',['index'=>'module_settings'])
+@extends('cms::layouts.mTabs',['index'=>'module_settings'])
 @section('tab')
     {!! "This is Media Settings Page" !!}
 @stop
